24d9c7316aa30018a307fd2efed13cc34712c3a0
Jiang Wang <billwang@billwang-desktop.(none)>
2011-05-06 15:02:41 -0500

    add file IO classes

---
+++ src/DepthMap.cpp
@@ -0,0 +1,139 @@
+#include <math.h>
+#include "DepthMap.h"
+
+
+CDepthMap::CDepthMap(void)
+{
+	m_ncols = 0;
+	m_nrows = 0;
+	m_depthVals = NULL;
+}
+
+CDepthMap::~CDepthMap(void)
+{
+	if(m_depthVals)
+	{
+		delete[] m_depthVals;
+		m_depthVals = NULL;
+	}
+}
+
+void CDepthMap::SetSize(int ncols, int nrows) //allocate space
+{
+	if(m_depthVals != NULL)
+	{
+		delete[] m_depthVals;
+		m_depthVals = NULL;
+	}
+	m_nrows = nrows;
+	m_ncols = ncols;
+	
+	m_depthVals = new float[nrows*ncols];
+	return;
+}
+
+void CDepthMap::SetItem(int r, int c, float val)
+{
+	m_depthVals[r * m_ncols + c] = val;
+}
+
+float CDepthMap::GetItem(int r, int c) const
+{
+	return m_depthVals[r * m_ncols + c] ;
+}
+
+//scale the depth values
+void CDepthMap::Scale(float s)
+{
+	int r,c;
+	for(r=0; r<GetNRows(); r++)
+	{
+		for(c=0; c<GetNCols(); c++)
+		{
+			float temp = GetItem(r,c);
+			SetItem(r,c, temp*s);
+		}
+	}
+}
+
+//scale the dimensions:
+//ncols = floor(scaleFactor * ncols), nrows = floor(scaleFactor * nrows)
+void CDepthMap::ScaleSizeNN(float scaleFactor)
+{
+	//buffer the original depthmap
+	CDepthMap bufDepthMap;
+	CopyDepthMapTo(bufDepthMap);
+
+	int new_ncols = floor(m_ncols * scaleFactor);
+	int new_nrows = floor(m_nrows * scaleFactor);
+
+	SetSize(new_ncols, new_nrows);
+
+	int r,c;
+	for(r=0; r<GetNRows(); r++)
+	{
+		for(c=0; c<GetNCols(); c++)
+		{
+			int origR = floor(r/scaleFactor);
+			int origC = floor(c/scaleFactor);
+			float origVal = bufDepthMap.GetItem(origR, origC);
+			SetItem(r,c, origVal);
+		}
+	}
+}
+
+void CDepthMap::CopyDepthMapTo(CDepthMap & dstMap) const
+{
+	dstMap.SetSize(m_ncols, m_nrows);
+	int r,c;
+	for(r=0; r<GetNRows(); r++)
+	{
+		for(c=0; c<GetNCols(); c++)
+		{
+			float temp = GetItem(r,c);
+			dstMap.SetItem(r,c, temp);
+		}
+	}
+}
+
+int CDepthMap::NumberOfNonZeroPoints() const
+{
+	int count=0;
+	int r,c;
+	for(r=0; r<GetNRows(); r++)
+	{
+		for(c=0; c<GetNCols(); c++)
+		{
+			float temp = GetItem(r,c);
+			if(temp != 0)
+			{
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+float CDepthMap::AvgNonZeroDepth() const
+{
+	int count=0;
+	float sum = 0;
+	int r,c;
+	for(r=0; r<GetNRows(); r++)
+	{
+		for(c=0; c<GetNCols(); c++)
+		{
+			float temp = GetItem(r,c);
+			if(temp != 0)
+			{
+				count++;
+				sum += temp;
+			}
+		}
+	}
+	if(count == 0)
+		return 0;
+	else
+		return sum/count;
+}
+
---
+++ src/DepthMap.h
@@ -0,0 +1,33 @@
+#ifndef CLASS_DEPTH_MAP
+#define CLASS_DEPTH_MAP
+
+//a depthmap is a matrix of depth values. A depth value is invalid if it is equal to 0 (i.e. the sensor has no data on this pixel).
+class CDepthMap
+{
+public:
+	CDepthMap(void);
+	~CDepthMap(void);
+
+	int GetNRows() const {return m_nrows;}
+	int GetNCols() const {return m_ncols;}
+
+	void SetSize(int ncols, int nrows); //allocate space
+
+	void SetItem(int r, int c, float val);
+	float GetItem(int r, int c) const;
+
+	void CopyDepthMapTo(CDepthMap & dstMap) const;
+
+	void Scale(float s); //scale the depth
+	void ScaleSizeNN(float scaleFactor); //ncols = floor(scaleFactor * ncols), nrows = floor(scaleFactor * nrows)
+
+	int NumberOfNonZeroPoints() const;
+	float AvgNonZeroDepth() const;
+
+
+protected:
+	int m_ncols; //ncols
+	int m_nrows; //nrows
+	float * m_depthVals; //nrows * ncols
+};
+#endif
