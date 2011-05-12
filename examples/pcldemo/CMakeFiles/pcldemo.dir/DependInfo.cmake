fa6011dc4ed2738af2efcfb3035ac5c42512ff64
Jiang Wang <billwang@billwang-desktop.(none)>
2011-05-12 16:14:15 -0500

    add pcl demo

---
+++ examples/pcldemo/CMakeFiles/pcldemo.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/billwang/work/NUKinect/examples/pcldemo/pcldemo.cpp" "/home/billwang/work/NUKinect/examples/pcldemo/CMakeFiles/pcldemo.dir/pcldemo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/billwang/work/NUKinect/src/CMakeFiles/NUKinect.dir/DependInfo.cmake"
+  )
---
+++ examples/pcldemo/CMakeFiles/pcldemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcldemo.dir/pcldemo.cpp.o"
+  "pcldemo.pdb"
+  "pcldemo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcldemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
